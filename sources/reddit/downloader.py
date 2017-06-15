f2cd60e2747d3c02430786d4a8da3fcc5548b1e3
tdabek <dabek.t@gmail.com>
2017-06-15 16:24:25 +0200

    Customizing JSON ouput for reddit for working with Hadoop

--- sources/reddit/downloader.py
+++ sources/reddit/downloader.py
@@ -1,19 +1,34 @@
-import praw
+import praw,re
 from cleanedObject import *
 
+#removes all source code from posts including code tag
+def removeSourceCodeWithCodeTag(text) :
+    return re.sub("((<code>).*(<\/code>))", '', text, flags = re.MULTILINE|re.DOTALL)
+
+#removes all tags from text
+def removeTags(text) :
+    return re.sub("(<.+?>)",'',text)
 
 def makeCleanedObjectFromSubmission(submission):
     cleanedObject = CleanedObject()
-    cleanedObject.body = submission.selftext
+    body = submission.selftext_html
+    body = removeSourceCodeWithCodeTag(body)
+    body = removeTags(body)
+    cleanedObject.body = body
     cleanedObject.author = submission.author.name
     cleanedObject.date = int(submission.created_utc)
+    cleanedObject.source = 2
     return cleanedObject
 
 def makeCleanedObjectFromComment(comment):
     cleanedObject = CleanedObject()
-    cleanedObject.body = comment.body
+    body = comment.body_html
+    body = removeSourceCodeWithCodeTag(body)
+    body = removeTags(body)
+    cleanedObject.body = body
     cleanedObject.author = comment.author.name
     cleanedObject.date = int(comment.created_utc)
+    cleanedObject.source = 2
     return cleanedObject
 
 def download(subredit,amount,file_path, id, secret,agent):
@@ -23,13 +38,12 @@ def download(subredit,amount,file_path, id, secret,agent):
     iterator = 1
     
     f = open(file_path,'w')
-    f.write('[')
     for submission in reddit.subreddit(subredit).hot(limit=amount):
         cleaned = makeCleanedObjectFromSubmission(submission)
         f.write(cleaned.toJSON())
         comment_iterator = 1
         if amount != iterator or submission.num_comments > 0:
-            f.write(',')
+            f.write('\n')
         submission.comments.replace_more(limit=0)
         comments = submission.comments.list()
         total_num_of_comments = len(comments)
@@ -37,10 +51,9 @@ def download(subredit,amount,file_path, id, secret,agent):
             cleaned = makeCleanedObjectFromComment(comment)
             f.write(cleaned.toJSON())
             if not (amount == iterator and comment_iterator ==  total_num_of_comments):
-                f.write(',')
+                f.write('\n')
             comment_iterator += 1
         iterator += 1
-    f.write(']')
     f.close()
 
 
